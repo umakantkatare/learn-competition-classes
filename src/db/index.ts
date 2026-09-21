6fe6682a993a901c2e3747a5991189d299e7b1b7
umakant katare <umakantkatare11@gmail.com>
2026-09-21 22:30:09 +0530

    fix issue

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,38 +1,7 @@
-import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
 
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-
-const globalForDb = globalThis as unknown as {
-  pool: Pool | undefined;
-};
-
-const pool =
-  globalForDb.pool ??
-  new Pool({
-    connectionString: databaseUrl,
-
-    max: 10,
-    idleTimeoutMillis: 30_000,
-    connectionTimeoutMillis: 10_000,
-
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: true }
-        : false,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.pool = pool;
-}
-
-export const db = drizzle({
-  client: pool,
-});
+export const db = drizzle(process.env.DATABASE_URL);
